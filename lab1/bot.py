68eb300651bbf7113fb984ed0d9f6e4fc6342fc8
mlvm <sk16729@essex.ac.uk>
2017-01-20 17:52:23 +0000

    lab1

---
+++ lab1/bot.py
@@ -0,0 +1,23 @@
+from sopel import module
+from emo.wdemotions import EmotionDetector
+
+emo = EmotionDetector()
+triggerlist ={}
+countlist={}
+
+
+@module.rule('')
+def hi(bot, trigger):
+    print(trigger, trigger.nick)
+	
+    bot.say('Hi, ' + trigger.nick)
+	
+	count = emo.detect_emotuion_in_row(str(trigger))
+	triggerlist.add(trigger)
+	countlist.add(count)
+	average=count/triggerlist.size()
+	print(average)
+
+	
+	
+	
